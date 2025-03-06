d8a2a1f0030cd04b63397923fd9483982e9a25d7
pawarpriyanka11 <140964866+pawarpriyanka11@users.noreply.github.com>
2025-03-06 14:41:13 +0530

    Add files via upload
    assignment 3

--- list.cpp
+++ list.cpp
@@ -1,87 +1,106 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Node {
-public:
-    int dest, cost;
-    Node* next;
-    Node(int d, int c) {
-        dest = d;
-        cost = c;
-        next = nullptr;
-    }
+class Node
+{
+   public:
+   int city;
+   int weight;
+   Node *next;
+   
+   Node(int city1,int w)
+   {
+     city=city1;
+     weight=w;
+     next=NULL;
+   }
 };
 
-class GRAPH {
-public:
-    int v, e;
-    string cities[5];
-    Node* adj[5] = {nullptr};
-    
-    GRAPH(int a, int b) {
-        v = a;
-        e = b;
-    }
 
-    void inputCities() {
-        cout << "\nEnter city names:" << endl;
-        for (int i = 0; i < v; i++) {
-            cin >> cities[i];
+class Graph
+{
+   public:
+     
+     string cities1[10];
+     void addCities(int v)
+     {
+        cout<<"\nEnter the cities:";
+        for(int i=0;i<v;i++)
+        {
+           cin>>cities1[i];
         }
-    }
-
-    int getCityIndex(string city) {
-        for (int i = 0; i < v; i++) {
-            if (cities[i] == city) return i;
+        for(int i=0;i<v;i++)
+        {
+         cout<<i<<" "<<cities1[i]<<endl;
         }
-        return -1;
+     }
+   int vert;
+   Node *ad[10];
+   
+   Graph(int i1)
+   {
+     vert=i1;
+     for(int i=0;i<vert;i++)
+     {
+      ad[i]=NULL;
+     }
+   }
+   
+   void addEdge(int s,int d,int w)
+   {
+      Node *newNode1=new Node(d,w);
+      newNode1->next=ad[s];
+      ad[s]=newNode1;
+      
+      Node *newNode2=new Node(s,w);
+      newNode2->next=ad[d];
+      ad[d]=newNode2;
+      
+      
+   }
+   
+   void display()
+   {
+    for(int i=0;i<vert;i++)
+    {
+      cout<<i<<"->";
+      Node *temp=ad[i];
+      while(temp!=NULL)
+      {
+        cout<<"("<<temp->city<<" "<<temp->weight<<")"<<" ";
+        temp=temp->next;
+      }
+       cout<<endl;
     }
-
-    void addEdge() {
-        string cityA, cityB;
-        int cost;
-        cout << "\nEnter source city, destination city, and cost (time/fuel):" << endl;
-        for (int i = 0; i < e; i++) {
-            cin >> cityA >> cityB >> cost;
-            int x = getCityIndex(cityA);
-            int y = getCityIndex(cityB);
-            if (x != -1 && y != -1) {
-                Node* newNode1 = new Node(y, cost);
-                newNode1->next = adj[x];
-                adj[x] = newNode1;
-                
-                Node* newNode2 = new Node(x, cost);
-                newNode2->next = adj[y];
-                adj[y] = newNode2;
-            }
-        }
-    }
-
-    void display() {
-        cout << "\nAdjacency List (Flight Costs):\n";
-        for (int i = 0; i < v; i++) {
-            cout << cities[i] << " -> ";
-            Node* temp = adj[i];
-            while (temp) {
-                cout << "(" << cities[temp->dest] << ", " << temp->cost << ") ";
-                temp = temp->next;
-            }
-            cout << endl;
-        }
-    }
+   
+   }
+   
 };
 
-int main() {
-    int n1, n2;
-    cout << "\nEnter the number of cities:";
-    cin >> n1;
-    cout << "\nEnter the number of flights:";
-    cin >> n2;
-    
-    GRAPH g(n1, n2);
-    g.inputCities();
-    g.addEdge();
-    g.display();
-    
-    return 0;
+int main()
+{
+int e1,cv,ss,dd,u,vx;
+
+
+cout<<"\nEnter the no of cities: ";
+cin>>dd;
+
+
+Graph g(dd);
+g.addCities(dd);
+cout<<"\nEnter the no of paths:";
+cin>>e1;
+for(int i=0;i<e1;i++)
+{
+ cout<<"\neneter edge (u,v) and weight:";
+ cin>>u>>vx>>cv;
+ g.addEdge(u,vx,cv);
+ }
+ 
+ 
+ cout<<"\ndisplay\n";
+ g.display();
+
+
+return 0;
 }
